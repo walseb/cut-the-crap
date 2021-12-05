74825aab9f57cb834e11fa71c368cb3351b218a4
Sebastian Wålinder <s.walinder@gmail.com>
2021-12-05 14:52:35 +0100

    Made compatible with davinci resolve, simplified shell.nix

--- shell.nix
+++ shell.nix
@@ -1,24 +1,16 @@
-{
-  config ? import ./nix/config.nix {},
-  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/haskell-packages.nix
-  pkgs ? import ./nix/pin.nix {
-    inherit config;
-} }:
+with import <nixpkgs> {};
 
 #  https://input-output-hk.github.io/haskell.nix/tutorials/development/
 pkgs.haskellPackages.shellFor {
   packages = ps : [ ps.cut-the-crap ];
   buildInputs = [
-        pkgs.ghcid
-        pkgs.haskellPackages.hasktags
-        pkgs.ffmpeg
-        pkgs.hlint
-        pkgs.pocketsphinx
-        pkgs.pkg-config
-        pkgs.cabal2nix
-        pkgs.haskellPackages.hpack
-        pkgs.haskellPackages.c2hs
-        ];
+    pkgs.ffmpeg
+    pkgs.hlint
+    pkgs.pocketsphinx
+    pkgs.pkg-config
+    pkgs.cabal2nix
+    pkgs.cabal-install
+  ];
   exactDeps = true;
   NIX_PATH="nixpkgs=${pkgs.path}:.";
 }
